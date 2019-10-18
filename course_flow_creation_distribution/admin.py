8c6c6e286ab900b898d3f4ced7e2c477ccb942e5
Arthur Ayestas Hilgert <aahilgert@gmail.com>
2019-10-18 16:17:23 -0400

    registered models in django admin

--- course_flow_creation_distribution/admin.py
+++ course_flow_creation_distribution/admin.py
@@ -1,5 +1,39 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import (
+    Course,
+    Preparation,
+    Activity,
+    Assesment,
+    Artifact,
+    Strategy,
+    Node,
+    LeftNodeIcon,
+    RightNodeIcon,
+    NodeClassification,
+    NodeStrategy,
+    StrategyActivity,
+    ComponentWeek,
+    WeekCourse,
+    Component,
+    Week,
+    Discipline,
+)
 
 admin.site.register(Course)
+admin.site.register(Discipline)
+admin.site.register(WeekCourse)
+admin.site.register(Week)
+admin.site.register(ComponentWeek)
+admin.site.register(Component)
+admin.site.register(Preparation)
+admin.site.register(Assesment)
+admin.site.register(Artifact)
+admin.site.register(Activity)
+admin.site.register(StrategyActivity)
+admin.site.register(Strategy)
+admin.site.register(NodeStrategy)
+admin.site.register(Node)
+admin.site.register(LeftNodeIcon)
+admin.site.register(RightNodeIcon)
+admin.site.register(NodeClassification)
