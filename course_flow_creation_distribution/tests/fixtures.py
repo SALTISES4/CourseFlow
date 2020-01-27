1fbc8edb29406680497a4266a09f23937b789ba6
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2020-01-27 14:22:00 -0500

    Added and tested lti

--- course_flow_creation_distribution/tests/fixtures.py
+++ course_flow_creation_distribution/tests/fixtures.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.conf import settings
+from lti import ToolConsumer
 import pytest
 
 from course_flow_creation_distribution.lti import generate_password
@@ -6,4 +8,26 @@ from course_flow_creation_distribution.lti import generate_password
 
 @pytest.fixture
 def user():
-    return User.objects.create_user(username="test", password=generate_password("test"))
+    return User.objects.create_user(
+        username="test", password=generate_password("test")
+    )
+
+
+@pytest.fixture
+def lti_consumer(user):
+    url = "http://testserver/lti/"
+    return ToolConsumer(
+        consumer_key=settings.LTI_CLIENT_KEY,
+        consumer_secret=settings.LTI_CLIENT_SECRET,
+        launch_url=url,
+        params={
+            "lti_message_type": "basic-lti-launch-request",
+            "lti_version": "1.0",
+            "resource_link_id": "0",
+            "context_id": "test",
+            "context_title": "test",
+            "lis_outcome_service_url": url,
+            "lis_result_sourcedid": url,
+            "user_id": user.username,
+        },
+    )
--- course_flow_creation_distribution/tests/test_lti.py
+++ course_flow_creation_distribution/tests/test_lti.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.models import User
 import pytest
+from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
+
 from course_flow_creation_distribution.lti import (
-    generate_password,
-    create,
     authenticate,
+    create,
+    generate_password,
 )
 
 
@@ -36,3 +37,56 @@ def test_generate_password():
     assert len(generate_password(username)) == 64
     assert generate_password(username) == generate_password(username)
     assert generate_password(username) != generate_password(username + "_")
+
+
+def test_lti(client, lti_consumer):
+    resp = client.post(
+        "http://testserver/lti/", data=lti_consumer.generate_launch_data()
+    )
+    assert resp.url == "/home/"
+
+
+def test_lti__course(client, lti_consumer):
+    course_id = 2
+    lti_consumer.launch_params["custom_course_id"] = str(course_id)
+    resp = client.post(
+        "http://testserver/lti/", data=lti_consumer.generate_launch_data()
+    )
+    assert resp.url == f"/course/{course_id}/"
+
+
+def test_lti__course_list(client, lti_consumer):
+    lti_consumer.launch_params["custom_course_list"] = "1"
+    resp = client.post(
+        "http://testserver/lti/", data=lti_consumer.generate_launch_data()
+    )
+    assert resp.url == f"/course-list/"
+
+
+def test_lti__new_user(client, lti_consumer):
+    User.objects.get(username=lti_consumer.launch_params["user_id"]).delete()
+    resp = client.post(
+        "http://testserver/lti/", data=lti_consumer.generate_launch_data()
+    )
+    assert resp.url == "/home/"
+    assert (
+        User.objects.get(username=lti_consumer.launch_params["user_id"])
+        is not None
+    )
+
+
+@pytest.mark.django_db(transaction=True)
+def test_lti__user_already_exists_wrong_password(client, lti_consumer, caplog):
+    User.objects.get(username=lti_consumer.launch_params["user_id"]).delete()
+    User.objects.create_user(
+        username=lti_consumer.launch_params["user_id"], password="wrong"
+    )
+    resp = client.post(
+        "http://testserver/lti/", data=lti_consumer.generate_launch_data()
+    )
+    assert resp.status_code == 404
+    assert caplog.records[0].getMessage() == (
+        "Lti tried to create a new user with username "
+        f"{lti_consumer.launch_params['user_id']}, but there already exists "
+        "a user with that username."
+    )
