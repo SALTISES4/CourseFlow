9937e996db979b3b83ec99fb82023bc005829693
Arthur Ayestas Hilgert <aahilgert@gmail.com>
2020-02-12 16:36:42 -0500

    added verbose_name to app

--- course_flow/apps.py
+++ course_flow/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class CourseFlowConfig(AppConfig):
     name = "course_flow"
+    verbose_name = "Course Flow"
 
     def ready(self):
         from django_lti_tool_provider.views import LTIView  # noqa
