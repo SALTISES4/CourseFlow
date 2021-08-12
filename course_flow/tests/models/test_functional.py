bc89958c3bb3f0e5dd45df8ca83c05578b57a7f8
Jeremie Choquette <jchoquette@dawsoncollege.qc.ca>
2021-08-12 16:44:58 -0700

    Attempting to fix test breaking only on circleci

--- course_flow/tests/models/test_functional.py
+++ course_flow/tests/models/test_functional.py
@@ -678,6 +678,10 @@ class SeleniumWorkflowsTestCase(StaticLiveServerTestCase):
             click_item = selenium.find_element_by_css_selector(
                 ".week .delete-self-button img"
             )
+            selenium.find_element_by_css_selector(
+                "#sidebar .window-close-button"
+            ).click()
+            time.sleep(0.5)
             action_hover_click(selenium, hover_item, click_item).perform()
             alert = wait.until(expected_conditions.alert_is_present())
             selenium.switch_to.alert.accept()
