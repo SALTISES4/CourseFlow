8f8f9584ce8b15b05667d9e571b917654d917c71
Jeremie Choquette <jchoquette@dawsoncollege.qc.ca>
2022-01-18 15:46:10 -0800

    Made consumers async

--- course_flow/consumers.py
+++ course_flow/consumers.py
@@ -1,18 +1,17 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .decorators import check_object_permission
 from .models import ObjectPermission, Workflow
 
 
-class WorkflowUpdateConsumer(WebsocketConsumer):
-    def connect(self):
+class WorkflowUpdateConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.workflow_pk = self.scope["url_route"]["kwargs"]["workflowPk"]
         self.room_group_name = "workflow_" + self.workflow_pk
         self.user = self.scope["user"]
-
+        
         try:
             workflow = Workflow.objects.get(pk=self.workflow_pk)
             self.VIEW = check_object_permission(
@@ -22,36 +21,35 @@ class WorkflowUpdateConsumer(WebsocketConsumer):
                 workflow, self.user, ObjectPermission.PERMISSION_EDIT
             )
         except:
-            return self.close()
+            return await self.close()
 
         if self.VIEW or self.EDIT:
-            async_to_sync(self.channel_layer.group_add)(
+            await self.channel_layer.group_add(
                 self.room_group_name, self.channel_name
             )
-            return self.accept()
-            
-        return self.close()
+            return await self.accept()
+        return await self.close()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
         try:
-            async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name,
                 {"type": "lock_update", "action": self.last_lock},
             )
         except AttributeError:
             pass
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         if not self.EDIT:
             return
         text_data_json = json.loads(text_data)
 
         if text_data_json["type"] == "micro_update":
             action = text_data_json["action"]
-            async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name,
                 {"type": "workflow_action", "action": action},
             )
@@ -59,33 +57,33 @@ class WorkflowUpdateConsumer(WebsocketConsumer):
             lock = text_data_json["lock"]
             if lock["lock"]:
                 self.last_lock = {**lock, "lock": False}
-            async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name, {"type": "lock_update", "action": lock}
             )
         elif text_data_json["type"] == "connection_update":
             user_data = text_data_json["user_data"]
-            async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 self.room_group_name,
                 {"type": "connection_update", "action": user_data},
             )
 
-    def workflow_action(self, event):
+    async def workflow_action(self, event):
         if not self.VIEW:
             return
         # Send message to WebSocket
         if event["type"] == "workflow_action":
-            self.send(text_data=json.dumps(event))
+            await self.send(text_data=json.dumps(event))
 
-    def lock_update(self, event):
+    async def lock_update(self, event):
         if not self.VIEW:
             return
         # Send message to WebSocket
         if event["type"] == "lock_update":
-            self.send(text_data=json.dumps(event))
+            await self.send(text_data=json.dumps(event))
 
-    def connection_update(self, event):
+    async def connection_update(self, event):
         if not self.VIEW:
             return
         # Send message to WebSocket
         if event["type"] == "connection_update":
-            self.send(text_data=json.dumps(event))
+            await self.send(text_data=json.dumps(event))
