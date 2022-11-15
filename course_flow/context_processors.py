75728f116ec6a00abec70d8a6b6bf3ea4decf90d
jchoquette <jeremie8@msn.com>
2022-11-15 10:09:31 -0500

    Fixed error when no update notifications have been created

--- course_flow/context_processors.py
+++ course_flow/context_processors.py
@@ -8,11 +8,10 @@ def update_notifications(request):
     updates = UpdateNotification.objects.order_by("-created_on")
     if updates.count() > 0:
         last_update = updates.first()
-        print(UpdateNotificationSerializer(last_update).data)
         last_update_serialized = (
             JSONRenderer()
             .render(UpdateNotificationSerializer(last_update).data)
             .decode("utf-8")
         )
         return {"update_notifications": last_update_serialized}
-    return {}
+    return {"update_notifications": {}}
