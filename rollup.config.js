a0614bd47e89f4409a9b29a61bb21bd5d30766b5
Aleksandar Nikolic <digrejzo@gmail.com>
2023-09-26 20:41:43 +0200

    rollup config codestyle cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,39 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import autoprefixer from 'autoprefixer';
-import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import postcss from "rollup-plugin-postcss";
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 import react from 'react';
 import reactDom from 'react-dom';
 
 const plugins = [
   postcss({
-    extensions: [".css"],
-    extract:'course_flow.css',
-    plugins:[
-      autoprefixer,
-    ],
+    extensions: ['.css'],
+    extract: 'course_flow.css',
+    plugins: [ autoprefixer ],
   }),
   resolve({
-    mainFields: ["browser", "module", "main"]
+    mainFields: ['browser', 'module', 'main']
   }),
   babel({
     babelrc: false,
-    exclude: ["node_modules/**"],
+    exclude: ['node_modules/**'],
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      ["@babel/plugin-transform-react-jsx"]
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-transform-react-jsx'
     ],
     presets: [
-      "@babel/preset-flow",
+      '@babel/preset-flow',
       [
-        "@babel/preset-env",
+        '@babel/preset-env',
         {
           targets: {
-            browsers: "last 4 versions and >=0.2% in CA and not ie <= 11"
+            browsers: 'last 4 versions and >=0.2% in CA and not ie <= 11'
           },
-          useBuiltIns: "usage",
+          useBuiltIns: 'usage',
           corejs: 3
         }
       ]
@@ -44,9 +42,9 @@ const plugins = [
   commonjs({
     include: 'node_modules/**',
     namedExports:{
-      'node_modules/react-is/index.js':['isValidElementType','isContextConsumer'],
+      'node_modules/react-is/index.js': ['isValidElementType', 'isContextConsumer'],
       'react': Object.keys(react),
-      'react-dom':Object.keys(reactDom),
+      'react-dom': Object.keys(reactDom),
     }
   }),
   terser()
@@ -54,43 +52,43 @@ const plugins = [
 
 export default [
   {
-    input: "course_flow/static/course_flow/js/scripts.js",
-    external:['jquery'],
+    input: 'course_flow/static/course_flow/js/scripts.js',
+    external: ['jquery'],
     output: {
-      file: "course_flow/static/course_flow/js/scripts.min.js",
-      name: "root",
-      format: "iife",
-      sourceMap: "inline"
+      file: 'course_flow/static/course_flow/js/scripts.min.js',
+      name: 'root',
+      format: 'iife',
+      sourceMap: 'inline'
     },
     plugins: plugins
   },
   {
-    input: "course_flow/static/course_flow/js/scripts-wf-redux.js",
+    input: 'course_flow/static/course_flow/js/scripts-wf-redux.js',
     output: {
-      file: "course_flow/static/course_flow/js/scripts-wf-redux.min.js",
-      name: "renderers",
-      format: "iife",
-      sourceMap: "inline"
+      file: 'course_flow/static/course_flow/js/scripts-wf-redux.min.js',
+      name: 'renderers',
+      format: 'iife',
+      sourceMap: 'inline'
     },
     plugins: plugins
   },
   {
-    input: "course_flow/static/course_flow/js/scripts-live.js",
+    input: 'course_flow/static/course_flow/js/scripts-live.js',
     output: {
-      file: "course_flow/static/course_flow/js/scripts-live.min.js",
-      name: "live_renderers",
-      format: "iife",
-      sourceMap: "inline"
+      file: 'course_flow/static/course_flow/js/scripts-live.min.js',
+      name: 'live_renderers',
+      format: 'iife',
+      sourceMap: 'inline'
     },
     plugins: plugins
   },
   {
-    input: "course_flow/static/course_flow/js/scripts-library.js",
+    input: 'course_flow/static/course_flow/js/scripts-library.js',
     output: {
-      file: "course_flow/static/course_flow/js/scripts-library.min.js",
-      name: "library_renderers",
-      format: "iife",
-      sourceMap: "inline"
+      file: 'course_flow/static/course_flow/js/scripts-library.min.js',
+      name: 'library_renderers',
+      format: 'iife',
+      sourceMap: 'inline'
     },
     plugins: plugins
   },
