6d66b01c734c61d626cb0ab65e2aa91e590e7d1b
Aleksandar Nikolic <digrejzo@gmail.com>
2023-10-04 14:53:14 +0200

    COUR-23 | Update rollup config to support blob entry points

--- rollup.config.js
+++ rollup.config.js
@@ -5,24 +5,27 @@ import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import multiEntry from '@rollup/plugin-multi-entry';
 import postcss from 'rollup-plugin-postcss';
 import react from 'react';
 import reactDom from 'react-dom';
 
+const templateBundleSrc = 'course_flow/static/course_flow/js/other/src/'
+const templateBundleDist = 'course_flow/static/course_flow/js/other/dist/'
 const bundleRoot = 'course_flow/static/course_flow/js/react/dist/'
+const bundleEntry = 'course_flow/static/course_flow/js/react/src/entry/'
 
-const plugins = [
-
+const plugins = {
   // bug in postCSS polugin does not allow paths outside of the bundleRoot
-  postcss({
+  postcss: postcss({
     extensions: ['.css'],
     extract: 'course_flow.css',
     plugins: [ autoprefixer ],
   }),
-  nodeResolve({
+  nodeResolve: nodeResolve({
     mainFields: ['browser', 'module', 'main']
   }),
-  babel({
+  babel: babel({
     babelrc: false,
     exclude: ['node_modules/**'],
     plugins: [
@@ -43,7 +46,7 @@ const plugins = [
       ]
     ]
   }),
-  commonjs({
+  commonjs: commonjs({
     include: 'node_modules/**',
     namedExports:{
       'node_modules/react-is/index.js': ['isValidElementType', 'isContextConsumer'],
@@ -51,49 +54,58 @@ const plugins = [
       'react-dom': Object.keys(reactDom),
     }
   }),
-  terser()
+  terser: terser()
+};
+
+const bundlePlugins = [
+  plugins.postcss,
+  plugins.nodeResolve,
+  plugins.babel,
+  plugins.commonjs,
+  plugins.terser
 ];
 
 export default [
   {
-    input: 'course_flow/static/course_flow/js/other/src/csrf-setup.js',
+    input: `${templateBundleSrc}**/*.js`,
     external: ['jquery'],
     output: {
-      file: 'course_flow/static/course_flow/js/other/dist/csrf-setup.min.js',
-      name: 'root',
-      format: 'iife',
-      sourceMap: 'inline'
+      dir: templateBundleDist,
+      preserveModules: true,
+      entryFileNames: '[name].min.js',
     },
-    plugins: plugins
+    plugins: [multiEntry({
+      preserveModules: true,
+    }), plugins.terser]
   },
   {
-    input: 'course_flow/static/course_flow/js/react/src/entry/scripts-wf-redux.js',
+    input: `${bundleEntry}scripts-wf-redux.js`,
     output: {
       file: `${bundleRoot}scripts-wf-redux.min.js`,
       name: 'renderers',
       format: 'iife',
       sourceMap: 'inline'
     },
-    plugins: plugins
+    plugins: bundlePlugins
   },
   {
-    input: 'course_flow/static/course_flow/js/react/src/entry/scripts-live.js',
+    input: `${bundleEntry}scripts-live.js`,
     output: {
       file: `${bundleRoot}scripts-live.min.js`,
       name: 'live_renderers',
       format: 'iife',
       sourceMap: 'inline'
     },
-    plugins: plugins
+    plugins: bundlePlugins
   },
   {
-    input: 'course_flow/static/course_flow/js/react/src/entry/scripts-library.js',
+    input: `${bundleEntry}scripts-library.js`,
     output: {
       file: `${bundleRoot}scripts-library.min.js`,
       name: 'library_renderers',
       format: 'iife',
       sourceMap: 'inline'
     },
-    plugins: plugins
+    plugins: bundlePlugins
   },
 ];
