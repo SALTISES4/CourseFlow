d25f607fb668f9c0f1905b2fa7fe2699e4ca327b
Aleksandar Nikolic <digrejzo@gmail.com>
2023-10-05 13:15:14 +0200

    JS codestyle cleanup

--- course_flow/static/course_flow/js/react/src/entry/scripts-live.js
+++ course_flow/static/course_flow/js/react/src/entry/scripts-live.js
@@ -1,58 +1,69 @@
 /**
- * @TODO why is this different than scripts-library.js
- */
-import * as reactDom from "react-dom";
-import * as React from "react";
-import {LiveProjectMenu, StudentLiveProjectMenu} from "../Components/Views/LiveProjectView.js";
-import {LiveAssignmentMenu} from "../Components/Views/LiveAssignmentView.js";
+* @TODO why is this different than scripts-library.js
+*/
+import * as reactDom from 'react-dom';
+import {
+  LiveProjectMenu,
+  StudentLiveProjectMenu
+} from '../Components/Views/LiveProjectView.js';
+import { LiveAssignmentMenu } from '../Components/Views/LiveAssignmentView.js';
 import 'flatpickr/dist/flatpickr.css';
 
+export class LiveProjectRenderer {
+  constructor(live_project_data, project_data) {
+    this.live_project_data = live_project_data;
+    this.project_data = project_data;
+    this.user_role = user_role;
+    this.user_permission = user_permission;
+  }
 
+  render(container) {
+    this.container = container;
+    this.tiny_loader = new renderers.TinyLoader($("body")[0]);
 
-export class LiveProjectRenderer{
-    constructor(live_project_data,project_data){
-        this.live_project_data = live_project_data;
-        this.project_data = project_data;
-        this.user_role = user_role;
-        this.user_permission = user_permission;
-    }
+    reactDom.render(this.getContents(), container[0]);
+  }
 
-    render(container){
-        this.container=container;
-        this.tiny_loader = new renderers.TinyLoader($("body")[0]);
-
-        reactDom.render(
-            this.getContents(),
-            container[0]
-        );
-    }
-
-    getContents(){
-        if(user_role === 2){
-            return <LiveProjectMenu renderer={this} project={this.project_data} liveproject={this.live_project_data}/>
-        }
-        return <StudentLiveProjectMenu renderer={this} project={this.project_data} liveproject={this.live_project_data}/>
-    }
+  getContents() {
+    return user_role === 2
+     ? (
+        <LiveProjectMenu
+          renderer={this}
+          project={this.project_data}
+          liveproject={this.live_project_data}
+        />
+      )
+    : (
+      <StudentLiveProjectMenu
+        renderer={this}
+        project={this.project_data}
+        liveproject={this.live_project_data}
+      />
+    )
+  }
 }
 
-export class LiveAssignmentRenderer{
-    constructor(assignment_data,live_project_data){
-        this.live_project_data = live_project_data;
-        this.assignment_data = assignment_data;
-        this.user_role = user_role;
-    }
+export class LiveAssignmentRenderer {
+  constructor(assignment_data, live_project_data) {
+    this.live_project_data = live_project_data;
+    this.assignment_data = assignment_data;
+    this.user_role = user_role;
+  }
 
-    render(container){
-        this.container=container;
-        this.tiny_loader = new renderers.TinyLoader($("body")[0]);
+  render(container) {
+    this.container = container;
+    this.tiny_loader = new renderers.TinyLoader($("body")[0]);
 
-        reactDom.render(
-            this.getContents(),
-            container[0]
-        );
-    }
+    reactDom.render(this.getContents(), container[0]);
+  }
 
-    getContents(){
-        return <LiveAssignmentMenu renderer={this} assignment_data={this.assignment_data} live_project_data={this.live_project_data}/>
-    }
+  getContents() {
+    return (
+      <LiveAssignmentMenu
+        renderer={this}
+        assignment_data={this.assignment_data}
+        live_project_data={this.live_project_data}
+      />
+    )
+  }
 }
