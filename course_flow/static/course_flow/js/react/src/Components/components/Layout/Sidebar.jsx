956a1cc74fe50690579aef411c626ae409223298
Aleksandar Nikolic <digrejzo@gmail.com>
2023-10-24 16:26:29 +0200

    COUR-37 | Add dummy sidebar component to test how plug-in React works

--- course_flow/static/course_flow/js/react/src/Components/components/Layout/Sidebar.jsx
+++ course_flow/static/course_flow/js/react/src/Components/components/Layout/Sidebar.jsx
@@ -1,10 +1,76 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import HomeIcon from '@mui/icons-material/Home'
+import FolderCopyIcon from '@mui/icons-material/FolderCopy'
+import SearchIcon from '@mui/icons-material/Search'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
 
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar here</h1>
-    </div>
+    <Box
+      sx={{
+        width: '256px'
+      }}
+    >
+      <Toolbar />
+
+      <Divider />
+
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Home'} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <FolderCopyIcon />
+            </ListItemIcon>
+            <ListItemText primary={'My Library'} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SearchIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Explore'} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CalendarMonthIcon />
+            </ListItemIcon>
+            <ListItemText primary={'My Classrooms'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+
+      <Divider />
+
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Favorites'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
   )
 }
 
--- course_flow/static/course_flow/js/react/src/entry/scripts-redesign.js
+++ course_flow/static/course_flow/js/react/src/entry/scripts-redesign.js
@@ -2,17 +2,67 @@
 // app and a place where all the code will be refactored/consolidated into
 // so that we end up with a single entry point into the frontend
 import React from 'react'
-import reactDom from 'react-dom'
 
+// https://mui.com/material-ui/react-css-baseline/#scoping-on-children
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+// required to be able to create a custom cache configuration that
+// will work with the provided Django nonce when injecting styles
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+
+// components
 import Sidebar from '../Components/components/Layout/Sidebar.jsx'
 
+// create the emotion cache
+const cache = createCache({
+  key: 'emotion',
+  nonce: document.querySelector('#script-redesign').nonce
+})
+
+// create the MUI theme
+// TODO: move this out potentially
+const theme = createTheme({
+  palette: {
+    success: {
+      main: '#ae0094'
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          color: theme.palette.success.main
+        }
+      }
+    }
+  }
+})
+
+// helper function that wraps each of the components we want to render
+// with an accompanying theme provider/css baseline since we're
+// progressively adding partials into the existing templates
+// TODO: cleanup this - injects providers for each component separately
 function renderComponentIntoNode(component, node) {
   const target = document.querySelector(node)
   if (target) {
-    reactDom.render(component, target)
+    const componentRoot = createRoot(target)
+    componentRoot.render(
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme}>
+          <ScopedCssBaseline>{component}</ScopedCssBaseline>
+        </ThemeProvider>
+      </CacheProvider>
+    )
   }
 }
 
+// Register all the components that we're loading ourselves on load
 window.addEventListener('load', () => {
-  renderComponentIntoNode(<Sidebar />, '.main-wrapper .left-panel')
+  renderComponentIntoNode(
+    <Sidebar />,
+    '.main-wrapper [data-component="sidebar"]'
+  )
 })
