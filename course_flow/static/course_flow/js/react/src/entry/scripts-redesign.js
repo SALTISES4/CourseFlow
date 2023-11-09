edbdbcb9f7cb4f4810fd80be5a1b1426bd761b98
Aleksandar Nikolic <digrejzo@gmail.com>
2023-11-09 11:41:31 +0100

    COUR-36 | Extract MUI theme to a separate file

--- course_flow/static/course_flow/js/react/src/entry/scripts-redesign.js
+++ course_flow/static/course_flow/js/react/src/entry/scripts-redesign.js
@@ -6,7 +6,8 @@ import React from 'react'
 // https://mui.com/material-ui/react-css-baseline/#scoping-on-children
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '../mui/theme.js'
 
 // required to be able to create a custom cache configuration that
 // will work with the provided Django nonce when injecting styles
@@ -24,33 +25,9 @@ const cache = createCache({
   nonce: document.querySelector('#script-redesign').nonce
 })
 
-// create the MUI theme
-// TODO: move this out potentially
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#04BA74',
-      light: '#52C68C',
-      dark: '#009E52'
-    },
-    secondary: {
-      main: '#78909C',
-      light: '#90A4AE',
-      dark: '#607D8B'
-    },
-    other: {
-      divider: '#CFD8DC'
-    }
-  },
-  typography: {
-    fontFamily: ['"Open Sans"', 'Helvetica', 'Arial', 'sans-serif'].join(',')
-  }
-})
-
 // helper function that wraps each of the components we want to render
 // with an accompanying theme provider/css baseline since we're
 // progressively adding partials into the existing templates
-// TODO: cleanup this - injects providers for each component separately
 function renderComponentIntoNode(component, node, styles = {}) {
   const target = document.querySelector(node)
   if (target) {
---
+++ course_flow/static/course_flow/js/react/src/mui/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material/styles'
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#04BA74',
+      light: '#52C68C',
+      dark: '#009E52'
+    },
+    secondary: {
+      main: '#78909C',
+      light: '#90A4AE',
+      dark: '#607D8B'
+    },
+    other: {
+      divider: '#CFD8DC'
+    }
+  },
+  typography: {
+    fontFamily: ['"Open Sans"', 'Helvetica', 'Arial', 'sans-serif'].join(',')
+  }
+})
