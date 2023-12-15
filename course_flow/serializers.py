fca0f84dd4976fc4def7ef76185430a0557de174
Aleksandar Nikolic <digrejzo@gmail.com>
2023-12-15 17:02:15 +0100

    COUR-273 | Create basic FormFieldsSerializer class to help retreive fields from a Form class

--- course_flow/serializers.py
+++ course_flow/serializers.py
@@ -1,6 +1,7 @@
 import datetime
 import re
 import time
+import json
 from functools import wraps
 
 import bleach
@@ -2140,7 +2141,47 @@ class NodeSerializerForAssignments(
             ).data
 
 
-#
+class FormFieldsSerializer:
+    def __init__(self, form_instance):
+        self.form_instance = form_instance
+
+    def get_field_type(self, field):
+        field_type = field.__class__.__name__
+        widget_type = field.widget.__class__.__name__
+
+        if field_type == "CharField":
+            return "text"
+        elif field_type == "IntegerField":
+            return "number"
+        elif field_type == "ChoiceField":
+            if widget_type == "Select":
+                return "select"
+            elif widget_type == "RadioSelect":
+                return "radio"
+        elif field_type == "BooleanField":
+            if widget_type == "CheckboxInput":
+                return "checkbox"
+        else:
+            return "text"
+
+    def prepare_fields(self):
+        fields = []
+
+        # have to check if the form instance is valid
+        # in order for cleaned_data to become available
+        if self.form_instance.is_valid():
+            for field_name, field in self.form_instance.fields.items():
+                fields.append({
+                    "name": field_name,
+                    "label": field.label,
+                    "type": self.get_field_type(field),
+                    "max_length": field.max_length if hasattr(field, 'max_length') else None,
+                    "help_text": field.help_text if hasattr(field, 'help_text') else None,
+                    "value": self.form_instance.cleaned_data.get(field_name, None),
+                })
+        return fields
+
+
 # serializer_lookups = {
 #    "node": NodeSerializer,
 #    "week": WeekSerializer,
