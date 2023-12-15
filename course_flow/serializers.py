c10ae75fb99f779b3ee9a0f4734ff9ecd1cbbf19
Aleksandar Nikolic <digrejzo@gmail.com>
2023-12-15 20:41:21 +0100

    COUR-273 | Tweak FormFieldsSerializer to better determine input type and also output options for the 'choice' inputs

--- course_flow/serializers.py
+++ course_flow/serializers.py
@@ -2145,12 +2145,17 @@ class FormFieldsSerializer:
     def __init__(self, form_instance):
         self.form_instance = form_instance
 
+    # figure out the appropriate html element input type
+    # based on combination of field and widget type
     def get_field_type(self, field):
         field_type = field.__class__.__name__
         widget_type = field.widget.__class__.__name__
 
         if field_type == "CharField":
-            return "text"
+            if getattr(field, "choices", None):
+                return "select" if widget_type == "Select" else "radio"
+        elif field_type == "TypedChoiceField":
+            return "radio" if widget_type == "RadioSelect" else "select"
         elif field_type == "IntegerField":
             return "number"
         elif field_type == "ChoiceField":
@@ -2161,8 +2166,19 @@ class FormFieldsSerializer:
         elif field_type == "BooleanField":
             if widget_type == "CheckboxInput":
                 return "checkbox"
-        else:
-            return "text"
+
+        return "text"
+
+    # generate the list of choices for fields which have them
+    def get_field_choices(self, field):
+        choices = []
+        if hasattr(field, 'choices'):
+            for choice in field.choices:
+                choices.append({
+                    "label": choice[1],
+                    "value": choice[0]
+                })
+        return choices if len(choices) > 0 else None
 
     def prepare_fields(self):
         fields = []
@@ -2173,8 +2189,9 @@ class FormFieldsSerializer:
             for field_name, field in self.form_instance.fields.items():
                 fields.append({
                     "name": field_name,
-                    "label": field.label,
+                    "label": field.label if hasattr(field, 'label') else None,
                     "type": self.get_field_type(field),
+                    "options": self.get_field_choices(field),
                     "max_length": field.max_length if hasattr(field, 'max_length') else None,
                     "help_text": field.help_text if hasattr(field, 'help_text') else None,
                     "value": self.form_instance.cleaned_data.get(field_name, None),
