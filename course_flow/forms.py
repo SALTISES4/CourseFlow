2195268a8393cee470ddb4998b93a7816d6ec88e
Aleksandar Nikolic <digrejzo@gmail.com>
2023-12-15 20:41:54 +0100

    COUR-273 | Update ProfileSettings form class

--- course_flow/forms.py
+++ course_flow/forms.py
@@ -23,27 +23,13 @@ class RegistrationForm(UserCreationForm):
 
 
 class ProfileSettings(forms.ModelForm):
-    # NOTE: If these two are omitted, the fields aren't required and the user can
-    # enter/save empty fields - does this mean User model should be tweaked
-    # or should we exclusively be working with the CourseFlowUser model?
-    # first_name = forms.CharField(label=_("First name"), max_length=30)
-    # last_name = forms.CharField(label=_("Last name"), max_length=30)
-
-    # TODO: Add language preferences field
-    # LANG_CHOICES = [
-    #     ("en", _("English")),
-    #     ("fr", _("French")),
-    # ]
-
-    # language = forms.ChoiceField(
-    #     label=_("Language preferences"),
-    #     choices=LANG_CHOICES,
-    #     widget=forms.RadioSelect(),
-    # )
-
     class Meta:
         model = CourseFlowUser
         fields = (
             "first_name",
             "last_name",
+            "language",
         )
+        widgets = {
+            'language': forms.RadioSelect,
+        }
