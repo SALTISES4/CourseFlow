e4e62dd1bc8fe953d877ab9d51d2f0cd04236afe
Aleksandar Nikolic <digrejzo@gmail.com>
2023-12-15 17:01:28 +0100

    COUR-273 | Create ProfileSettings django form class

--- course_flow/forms.py
+++ course_flow/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
+from .models import CourseFlowUser
 
 
 class RegistrationForm(UserCreationForm):
@@ -21,16 +22,28 @@ class RegistrationForm(UserCreationForm):
         )
 
 
-class ProfileSettings(forms.Form):
-    first_name = forms.CharField(label=_("First name"), max_length=30)
-    last_name = forms.CharField(label=_("Last name"), max_length=30)
+class ProfileSettings(forms.ModelForm):
+    # NOTE: If these two are omitted, the fields aren't required and the user can
+    # enter/save empty fields - does this mean User model should be tweaked
+    # or should we exclusively be working with the CourseFlowUser model?
+    # first_name = forms.CharField(label=_("First name"), max_length=30)
+    # last_name = forms.CharField(label=_("Last name"), max_length=30)
 
-    LANG_CHOICES = [
-        ("en", _("English")),
-        ("fr", _("French")),
-    ]
+    # TODO: Add language preferences field
+    # LANG_CHOICES = [
+    #     ("en", _("English")),
+    #     ("fr", _("French")),
+    # ]
 
-    language = forms.ChoiceField(
-        choices=LANG_CHOICES,
-        widget=forms.RadioSelect(),
-    )
+    # language = forms.ChoiceField(
+    #     label=_("Language preferences"),
+    #     choices=LANG_CHOICES,
+    #     widget=forms.RadioSelect(),
+    # )
+
+    class Meta:
+        model = CourseFlowUser
+        fields = (
+            "first_name",
+            "last_name",
+        )
