b8349ba8e7559d68509f109a805cf282f1da5c56
Alexis Dray <alexis@littlebox.io>
2023-12-17 16:23:55 -0500

    get rid of autocompiled JS

--- course_flow/static/course_flow/js/react/src/Components/components/CommonComponents/UIComponents/WorkflowLoader.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from 'react/jsx-runtime'
-import React from 'react'
-const WorkflowLoader = () => {
-  return _jsx('div', { className: 'load-screen' })
-}
-export default WorkflowLoader
