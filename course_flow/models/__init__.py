673ce29a41884800e26158c4e9307b4101a67d07
Alexis Dray <alexis@littlebox.io>
2024-01-03 14:33:40 -0500

    test sort order

--- course_flow/models/__init__.py
+++ course_flow/models/__init__.py
@@ -16,10 +16,20 @@ from .objectset import ObjectSet
 from .outcome import Outcome
 from .program import Program
 from .project import Project
-from .relations import *
 from .updateNotification import UpdateNotification
 from .userAssignment import UserAssignment
 from .week import Week
 from .workflow import Workflow
+
+from .relations.columnWorkflow import ColumnWorkflow
+from .relations.liveProjectUser import LiveProjectUser
+from .relations.nodeLink import NodeLink
+from .relations.nodeWeek import NodeWeek
+from .relations.outcomeHorizontalLink import OutcomeHorizontalLink
+from .relations.outcomeNode import OutcomeNode
+from .relations.outcomeOutcome import OutcomeOutcome
+from .relations.outcomeWorkflow import OutcomeWorkflow
+from .relations.weekWorkflow import WeekWorkflow
+from .relations.workflowProject import WorkflowProject
 
 User = get_user_model()
