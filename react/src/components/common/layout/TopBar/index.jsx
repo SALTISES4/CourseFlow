2955aa669d8efc5e683be5c8a2594d8e459613d8
Aleksandar Nikolic <digrejzo@gmail.com>
2024-01-11 13:33:19 +0100

    Extract TopBar styles into a separate file

--- react/src/components/common/layout/TopBar/index.jsx
+++ react/src/components/common/layout/TopBar/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { styled } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
@@ -8,13 +7,10 @@ import IconButton from '@mui/material/IconButton'
 import Link from '@mui/material/Link'
 import Badge from '@mui/material/Badge'
 import MenuItem from '@mui/material/MenuItem'
-import Menu from '@mui/material/Menu'
-import Popover from '@mui/material/Popover'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import LogoutIcon from '@mui/icons-material/Logout'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
-import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
@@ -26,63 +22,13 @@ import { createNew } from '@XMLHTTP/postTemp'
 
 import ResetPasswordModal from './components/ResetPasswordModal'
 
-const TopBarWrap = styled(Box)(({ theme }) => ({
-  '& .MuiPaper-root': {
-    backgroundColor: theme.palette.common.white
-  }
-}))
-
-const StyledMenu = styled(Menu)(({ theme }) => ({
-  '& .MuiPaper-root': {
-    minWidth: 220,
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        marginRight: theme.spacing(1.5)
-      }
-    }
-  }
-}))
-
-const NotificationsMenu = styled(Popover)({
-  '& .MuiPaper-root': {
-    marginLeft: '3em',
-    width: 500
-  }
-})
-
-const NotificationsHeader = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  paddingTop: theme.spacing(2),
-  paddingBottom: theme.spacing(2),
-  paddingLeft: theme.spacing(3),
-  paddingRight: theme.spacing(1),
-  borderBottom: `1px solid ${theme.palette.divider}`,
-  '& .MuiTypography-root:not(a)': {
-    color: 'currentColor'
-  }
-}))
-
-const NotificationsList = styled(List)(({ theme }) => ({
-  paddingTop: 0,
-  paddingBottom: 0,
-  marginBottom: theme.spacing(1),
-  '& .MuiListItem-root': {
-    padding: 0
-  },
-  '& .MuiListItemButton-root': {
-    paddingTop: theme.spacing(1.5),
-    paddingBottom: theme.spacing(1.5),
-    paddingLeft: theme.spacing(4),
-    borderBottom: `1px solid ${theme.palette.divider}`
-  },
-  '& .MuiBadge-root': {
-    position: 'absolute',
-    left: theme.spacing(1.7),
-    top: '50%'
-  }
-}))
+import {
+  TopBarWrap,
+  StyledMenu,
+  NotificationsMenu,
+  NotificationsHeader,
+  NotificationsList
+} from './styles'
 
 const TopBar = () => {
   const [anchorEl, setAnchorEl] = useState(null)
