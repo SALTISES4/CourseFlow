4e85866f09e0d72b8c00b15047a43ed877cf06fe
Aleksandar Nikolic <digrejzo@gmail.com>
2024-07-28 09:53:30 +0200

    Cleanup Styleguide page

--- react/src/components/pages/Styleguide/index.tsx
+++ react/src/components/pages/Styleguide/index.tsx
@@ -1,12 +1,9 @@
 import { OuterContentWrap } from '@cfModule/mui/helper'
 
-import SectionDialogs from './SectionDialogs'
-
 const Styleguide = () => {
   return (
     <OuterContentWrap>
       <h1>Courseflow Styleguide</h1>
-      <SectionDialogs />
     </OuterContentWrap>
   )
 }
