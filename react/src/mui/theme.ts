5ee3088127cd9be4c1710cc18788133ad08f7d23
Aleksandar Nikolic <digrejzo@gmail.com>
2024-08-30 18:34:54 +0200

    Update MUI theme

--- react/src/mui/theme.ts
+++ react/src/mui/theme.ts
@@ -1,39 +1,40 @@
 import { Theme, createTheme } from '@mui/material/styles'
 
-const colors = {
-  primary: {
-    main: '#04BA74',
-    light: '#52C68C',
-    dark: '#009E52',
-    contrastText: '#fff'
-  },
-  secondary: {
-    main: '#78909C',
-    light: '#90A4AE',
-    dark: '#607D8B'
-  },
-  courseflow: {
-    lightest: '#e2f5eb',
-    favouriteActive: 'rgba(255, 180, 0, 1)',
-    favouriteInactive: 'rgba(0, 0, 0, 0.23)',
-    project: 'rgba(245, 127, 23, 1)',
-    program: 'rgba(0, 105, 92, 1)',
-    course: 'rgba(183, 28, 28, 1)',
-    activity: 'rgba(41, 98, 255, 1)',
-    template: 'rgba(255, 64, 129, 1)'
-  }
-}
-
 const theme: Theme = createTheme({
   palette: {
     primary: {
-      ...colors.primary
+      main: '#04BA74',
+      light: '#52C68C',
+      dark: '#009E52',
+      contrastText: '#fff'
     },
     secondary: {
-      ...colors.secondary
+      main: '#78909C',
+      light: '#90A4AE',
+      dark: '#607D8B'
     },
     courseflow: {
-      ...colors.courseflow
+      lightest: '#e2f5eb',
+      favouriteActive: 'rgba(255, 180, 0, 1)',
+      favouriteInactive: 'rgba(0, 0, 0, 0.23)',
+      project: 'rgba(245, 127, 23, 1)',
+      program: 'rgba(0, 105, 92, 1)',
+      course: 'rgba(183, 28, 28, 1)',
+      activity: 'rgba(41, 98, 255, 1)',
+      template: 'rgba(255, 64, 129, 1)'
+    },
+    workspaceBlocks: {
+      courseAssessment: '#ad1d35',
+      courseProject: '#ed4a28',
+      courseLesson: '#ed8934',
+      coursePreparation: '#f7b92a',
+      reusableBlocks: '#b388ff',
+      strategies: '#651fff',
+      activityOOCInstr: '#08118a',
+      activityOOCStud: '#114bd4',
+      activityICInstr: '#268ae5',
+      activityICStud: '#8bc8ff',
+      background: '#EEF2FD'
     },
     divider: '#CFD8DC',
     action: {
@@ -52,11 +53,18 @@ const theme: Theme = createTheme({
         }
       }
     },
-    MuiAppBar: {
-      // Adding MuiAppBar to the theme overrides
+    MuiTab: {
       styleOverrides: {
         root: {
-          color: colors.secondary.dark // Ensuring text color is white, adjust as needed
+          textTransform: 'none',
+          fontWeight: '600'
+        }
+      }
+    },
+    MuiStepIcon: {
+      styleOverrides: {
+        root: {
+          color: 'rgba(0, 0, 0, 0.12)'
         }
       }
     }
