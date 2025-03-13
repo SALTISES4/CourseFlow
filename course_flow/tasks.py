389aad55294d416df772635bae7703623294676f
JasonJ0987 <chiyoung.jang@mail.mcgill.ca>
2025-03-13 11:25:23 -0400

    small changes

--- course_flow/tasks.py
+++ course_flow/tasks.py
@@ -39,7 +39,7 @@ def async_send_export_email(
 
     #Jason tester
     if export_type == "jexcel":
-        print(excel_export.get_export_analytics(model_object))
+        excel_export.get_analytics_table(model_object, export_format)
 
 
 
