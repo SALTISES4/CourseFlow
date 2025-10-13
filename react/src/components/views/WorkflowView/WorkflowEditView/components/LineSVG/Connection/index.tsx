1c278558094363c1eb2856d7f16441a953321e0c
Aleksandar Nikolic <digrejzo@gmail.com>
2025-10-13 15:22:45 +0200

    COURSEFLOW-460 | Extract Connection from the LineSVG component

---
+++ react/src/components/views/WorkflowView/WorkflowEditView/components/LineSVG/Connection/index.tsx
@@ -0,0 +1,117 @@
+import { Position, getSmoothStepPath } from '@xyflow/react'
+import { produce } from 'immer'
+import { MutableRefObject, useCallback, useState } from 'react'
+
+import { ConnectionType } from '../types'
+import { getCoords } from '../utility'
+
+type ConnectionState = {
+  hovering: boolean
+  selected: boolean
+  highlighted: boolean
+}
+
+const Connection = ({
+  id,
+  from,
+  to,
+  offset,
+  svgRef
+}: ConnectionType & {
+  svgRef: MutableRefObject<SVGSVGElement | null>
+}) => {
+  const [state, setState] = useState<ConnectionState>({
+    hovering: false,
+    selected: false,
+    highlighted: false
+  })
+
+  const toggleHover = useCallback(() => {
+    setState(
+      produce((draft) => {
+        draft.hovering = !draft.hovering
+      })
+    )
+  }, [])
+
+  const toggleSelected = useCallback(() => {
+    setState(
+      produce((draft) => {
+        draft.selected = !draft.selected
+      })
+    )
+  }, [])
+
+  let strokeColor = '#666' // actually '#CBD5DF'
+  if (state.hovering) {
+    strokeColor = '#7CD5B9'
+  }
+  if (state.highlighted || state.selected) {
+    strokeColor = '#FCD748'
+  }
+
+  const [fromId, fromEdge] = from
+  const [toId, toEdge] = to
+
+  const fromEl = document.getElementById(`node-${fromId}`)
+  const toEl = document.getElementById(`node-${toId}`)
+
+  if (!fromEl || !toEl || !svgRef?.current) {
+    return null
+  }
+
+  const svgBCR = svgRef.current.getBoundingClientRect()
+  const fromBCR = fromEl.getBoundingClientRect()
+  const toBCR = toEl.getBoundingClientRect()
+
+  const lineStart = getCoords(fromBCR, fromEdge)
+  const lineEnd = getCoords(toBCR, toEdge)
+
+  // adjust positions for the SVG BCR and window scrolling
+  lineStart.x += window.scrollX - svgBCR.left + offset.from.x
+  lineStart.y += window.scrollY - svgBCR.top + offset.from.y
+
+  lineEnd.x += window.scrollX - svgBCR.left + offset.to.x
+  lineEnd.y += window.scrollY - svgBCR.top + offset.to.y
+
+  const [path, labelX, labelY] = getSmoothStepPath({
+    sourceX: lineStart.x,
+    sourceY: lineStart.y,
+    sourcePosition: fromEdge as Position,
+    targetX: lineEnd.x,
+    targetY: lineEnd.y,
+    targetPosition: toEdge as Position
+  })
+
+  const lineId = `line-${fromId}-${fromEdge}-to-${toId}-${toEdge}`
+
+  return (
+    <g fill="none" data-nodelink-id={id}>
+      <path
+        d={path}
+        stroke={strokeColor}
+        strokeWidth={state.selected ? 3 : 1}
+        fill="none"
+        markerEnd="url(#line-arrow)"
+      />
+      <path
+        id={lineId}
+        d={path}
+        stroke="transparent"
+        strokeWidth="16"
+        fill="none"
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+        onClick={toggleSelected}
+        style={{ pointerEvents: 'auto', cursor: 'pointer' }}
+      />
+      {state.selected && (
+        <text x={labelX} y={labelY} fill="red">
+          Line text label
+        </text>
+      )}
+    </g>
+  )
+}
+
+export default Connection
--- react/src/components/views/WorkflowView/WorkflowEditView/components/LineSVG/index.tsx
+++ react/src/components/views/WorkflowView/WorkflowEditView/components/LineSVG/index.tsx
@@ -1,19 +1,17 @@
-import { selectAllNodelink } from '@cfRedux/selectors/nodelink.selector'
-import { Position, getSmoothStepPath } from '@xyflow/react'
-import { produce } from 'immer'
-import { MutableRefObject, useCallback, useRef, useState } from 'react'
+import { selectActiveLinks } from '@cfRedux/selectors/nodelink.selector'
+import { useRef } from 'react'
 import { useSelector } from 'react-redux'
 
-import DrawPreview from './DrawPreview'
+import Connection from './Connection'
 import { edgeKeys } from './types'
 import { ConnectionType } from './types'
-import { generateOffsets, getCoords, groupLinksByNodeEdge } from './utility'
+import { generateOffsets, groupLinksByNodeEdge } from './utility'
 
 const LineSVG = () => {
   const ref = useRef<SVGSVGElement>(null)
 
   // grab all the non-deleted links
-  const links = useSelector(selectAllNodelink).filter((link) => !link.deleted)
+  const links = useSelector(selectActiveLinks)
 
   // group links into node/edge to allow offsets
   const linksGroup = groupLinksByNodeEdge(links)
@@ -101,115 +99,6 @@ const LineSVG = () => {
         }}
       /> */}
     </svg>
-  )
-}
-
-type ConnectionState = {
-  hovering: boolean
-  selected: boolean
-  highlighted: boolean
-}
-
-const Connection = ({
-  id,
-  from,
-  to,
-  offset,
-  svgRef
-}: ConnectionType & {
-  svgRef: MutableRefObject<SVGSVGElement | null>
-}) => {
-  const [state, setState] = useState<ConnectionState>({
-    hovering: false,
-    selected: false,
-    highlighted: false
-  })
-
-  const toggleHover = useCallback(() => {
-    setState(
-      produce((draft) => {
-        draft.hovering = !draft.hovering
-      })
-    )
-  }, [])
-
-  const toggleSelected = useCallback(() => {
-    setState(
-      produce((draft) => {
-        draft.selected = !draft.selected
-      })
-    )
-  }, [])
-
-  let strokeColor = '#666' // actually '#CBD5DF'
-  if (state.hovering) {
-    strokeColor = '#7CD5B9'
-  }
-  if (state.highlighted || state.selected) {
-    strokeColor = '#FCD748'
-  }
-
-  const [fromId, fromEdge] = from
-  const [toId, toEdge] = to
-
-  const fromEl = document.getElementById(`node-${fromId}`)
-  const toEl = document.getElementById(`node-${toId}`)
-
-  if (!fromEl || !toEl || !svgRef?.current) {
-    return null
-  }
-
-  const svgBCR = svgRef.current.getBoundingClientRect()
-  const fromBCR = fromEl.getBoundingClientRect()
-  const toBCR = toEl.getBoundingClientRect()
-
-  const lineStart = getCoords(fromBCR, fromEdge)
-  const lineEnd = getCoords(toBCR, toEdge)
-
-  // adjust positions for the SVG BCR and window scrolling
-  lineStart.x += window.scrollX - svgBCR.left + offset.from.x
-  lineStart.y += window.scrollY - svgBCR.top + offset.from.y
-
-  lineEnd.x += window.scrollX - svgBCR.left + offset.to.x
-  lineEnd.y += window.scrollY - svgBCR.top + offset.to.y
-
-  const [path, labelX, labelY] = getSmoothStepPath({
-    sourceX: lineStart.x,
-    sourceY: lineStart.y,
-    sourcePosition: fromEdge as Position,
-    targetX: lineEnd.x,
-    targetY: lineEnd.y,
-    targetPosition: toEdge as Position
-  })
-
-  const lineId = `line-${fromId}-${fromEdge}-to-${toId}-${toEdge}`
-
-  return (
-    <g fill="none" data-nodelink-id={id}>
-      <path
-        d={path}
-        stroke={strokeColor}
-        strokeWidth={state.selected ? 3 : 1}
-        fill="none"
-        markerEnd="url(#line-arrow)"
-      />
-      <path
-        id={lineId}
-        d={path}
-        stroke="transparent"
-        strokeWidth="16"
-        fill="none"
-        onMouseEnter={toggleHover}
-        onMouseLeave={toggleHover}
-        onClick={toggleSelected}
-        style={{ pointerEvents: 'auto', cursor: 'pointer' }}
-      />
-      {state.selected && (
-        <text x={labelX} y={labelY} fill="red">
-          Line text label
-        </text>
-      )}
-    </g>
   )
 }
 
