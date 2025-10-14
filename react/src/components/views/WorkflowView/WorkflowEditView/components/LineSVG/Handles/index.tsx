0e7777e7c70aff84e6a95f0e0918a9899bfd63be
Aleksandar Nikolic <digrejzo@gmail.com>
2025-10-14 12:57:29 +0200

    COURSEFLOW-460 | Begin working on node handles, clean up callbacks

--- react/src/components/views/WorkflowView/WorkflowEditView/components/LineSVG/Handles/index.tsx
+++ react/src/components/views/WorkflowView/WorkflowEditView/components/LineSVG/Handles/index.tsx
@@ -1,3 +1,4 @@
+import { Position } from '@xyflow/react'
 import { produce } from 'immer'
 import { MouseEvent as ReactMouseEvent, useCallback, useState } from 'react'
 
@@ -9,14 +10,18 @@ type PropsType = {
   diameter?: number
 }
 
-type CoordsType = {
-  from: { x: number; y: number }
-  to: { x: number; y: number }
+type DragCoords = {
+  x: number
+  y: number
+  edge: Position
 }
 
 type StateType = {
   dragging: boolean
-  coords: CoordsType | null
+  coords: {
+    from: DragCoords
+    to: DragCoords
+  } | null
 }
 
 const Handles = ({ id, hovering, diameter = 10 }: PropsType) => {
@@ -25,66 +30,72 @@ const Handles = ({ id, hovering, diameter = 10 }: PropsType) => {
     coords: null
   })
 
-  const onMouseDown = useCallback((e: ReactMouseEvent<SVGCircleElement>) => {
-    e.stopPropagation()
-    e.preventDefault()
+  const onMouseDown = useCallback((edge: Position) => {
+    return (e: ReactMouseEvent<SVGCircleElement>) => {
+      e.stopPropagation()
+      e.preventDefault()
 
-    const target = e.currentTarget
-    const bcr = target.getBoundingClientRect()
-
-    setState({
-      dragging: true,
-      coords: {
-        from: { x: bcr.x + bcr.width / 2, y: bcr.y + bcr.height / 2 },
-        to: { x: bcr.x + bcr.width / 2, y: bcr.y + bcr.height / 2 }
+      const target = e.currentTarget
+      const bcr = target.getBoundingClientRect()
+      const cursor = {
+        x: bcr.x + bcr.width / 2,
+        y: bcr.y + bcr.height / 2,
+        edge
       }
-    })
 
-    const onMouseMove = (e: MouseEvent) => {
-      setState(
-        produce((draft) => {
-          draft.dragging = true
-          draft.coords.to = { x: e.clientX, y: e.clientY }
-        })
-      )
+      setState({
+        dragging: true,
+        coords: { from: cursor, to: cursor }
+      })
+
+      const onMouseMove = (e: MouseEvent) => {
+        console.log('move to', { x: e.clientX, y: e.clientY })
+        setState(
+          produce((draft) => {
+            draft.dragging = true
+            draft.coords.to = {
+              x: e.clientX,
+              y: e.clientY,
+              edge: Position.Left
+            }
+          })
+        )
+      }
+
+      const onMouseUp = () => {
+        window.removeEventListener('mousemove', onMouseMove)
+        window.removeEventListener('mouseup', onMouseUp)
+        setState({ dragging: false, coords: null })
+      }
+
+      window.addEventListener('mousemove', onMouseMove)
+      window.addEventListener('mouseup', onMouseUp)
     }
-
-    const onMouseUp = () => {
-      window.removeEventListener('mousemove', onMouseMove)
-      window.removeEventListener('mouseup', onMouseUp)
-      setState({ dragging: false, coords: null })
-    }
-
-    window.addEventListener('mousemove', onMouseMove)
-    window.addEventListener('mouseup', onMouseUp)
   }, [])
 
   const r = diameter / 2
 
-  const handles = [
-    ['50%', '0%', `translate(0, ${r})`], // top
-    ['100%', '50%', `translate(-${r}, 0)`], // right
-    ['50%', '100%', `translate(0, -${r})`], // bottom
-    ['0%', '50%', `translate(${r}, 0)`] // left
+  const handles: [string, string, string, Position][] = [
+    ['50%', '0%', `translate(0, ${r})`, Position.Top],
+    ['100%', '50%', `translate(-${r}, 0)`, Position.Right],
+    ['50%', '100%', `translate(0, -${r})`, Position.Bottom],
+    ['0%', '50%', `translate(${r}, 0)`, Position.Left]
   ]
 
-  return (
-    hovering && (
-      <Styled.Wrap radius={r}>
-        {handles.map(([cx, cy, transform], index) => (
-          <Styled.Handle
-            sx={{ pointerEvents: state.dragging ? 'none' : 'auto' }}
-            key={index}
-            cx={cx}
-            cy={cy}
-            r={r}
-            transform={transform}
-            onMouseDown={onMouseDown}
-          />
-        ))}
-      </Styled.Wrap>
-    )
-  )
+  return hovering ? (
+    <Styled.Wrap radius={r}>
+      {handles.map(([cx, cy, transform, edge], index) => (
+        <Styled.Handle
+          key={index}
+          cx={cx}
+          cy={cy}
+          r={r}
+          transform={transform}
+          onMouseDown={onMouseDown(edge)}
+        />
+      ))}
+    </Styled.Wrap>
+  ) : null
 }
 
 export default Handles
