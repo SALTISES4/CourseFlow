0532067b05f475d7f91cd9da878d3d3a8f1373d4
Aleksandar Nikolic <digrejzo@gmail.com>
2026-01-25 09:57:58 +0100

    Cleanup | Remove unnecessary import

--- react/src/components/pages/Workspace/Workflow/Sidebar/components/AddTab/Draggable/styles.ts
+++ react/src/components/pages/Workspace/Workflow/Sidebar/components/AddTab/Draggable/styles.ts
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box'
 import { blueGrey, yellow } from '@mui/material/colors'
-import IconButton from '@mui/material/IconButton'
 import { styled } from '@mui/material/styles'
 
 export const DraggableWrap = styled(Box, {
