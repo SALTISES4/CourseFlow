84d4fafb42c2915b1f23eab95065a48ba5f247d7
Aleksandar Nikolic <digrejzo@gmail.com>
2026-02-12 17:08:53 +0100

    Clean up Favourite (star icon) functionality

--- react/src/components/common/UIPrimitives/Favourite/index.tsx
+++ react/src/components/common/UIPrimitives/Favourite/index.tsx
@@ -1,9 +1,10 @@
 import { LibraryObjectType } from '@cf/types/enum'
+import { _t } from '@cf/utility/Utility.class'
 import StarIcon from '@mui/icons-material/Star'
 import IconButton from '@mui/material/IconButton'
 import { useToggleFavouriteMutation } from '@XMLHTTP/API/library.rtk'
 import { enqueueSnackbar } from 'notistack'
-import { useEffect, useState } from 'react'
+import { MouseEvent, useCallback, useEffect, useState } from 'react'
 
 type PropsType = {
   id: number
@@ -14,47 +15,48 @@ type PropsType = {
 const Favourite = ({ id, isFavourite, type }: PropsType) => {
   const [isFavouriteState, setFavouriteState] = useState<boolean>(isFavourite)
 
-  const [toggleMutate, { isError, error, isSuccess }] =
-    useToggleFavouriteMutation()
+  const [toggleMutate] = useToggleFavouriteMutation()
 
-  function onSuccess() {
+  const onSuccess = useCallback(() => {
     setFavouriteState(!isFavouriteState)
     enqueueSnackbar('Success toggling favourites', {
       variant: 'success'
     })
-  }
+  }, [isFavouriteState])
 
-  function onError(error) {
+  const onError = useCallback((error) => {
     console.error('Error updating toggle:', error)
     enqueueSnackbar('Error toggling favourites', {
       variant: 'error'
     })
-  }
+  }, [])
 
-  useEffect(() => {
-    if (isError) {
-      onError(error)
-    }
-    if (isSuccess) {
-      onSuccess()
-    }
-  }, [isError, error, isSuccess])
+  const onStarClick = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation()
+      e.preventDefault()
+
+      toggleMutate({
+        id,
+        objectType: type,
+        favourite: !isFavouriteState
+      })
+        .unwrap()
+        .then(() => onSuccess())
+        .catch((err) => onError(err))
+    },
+    [id, type, isFavouriteState, onError, onSuccess, toggleMutate]
+  )
 
   return (
     <IconButton
-      aria-label="Favourite"
+      aria-label={_t('Favourite')}
       sx={{
         color: isFavouriteState
           ? 'courseflow.favouriteActive'
           : 'courseflow.favouriteInactive'
       }}
-      onClick={() =>
-        toggleMutate({
-          id,
-          objectType: type,
-          favourite: !isFavouriteState
-        })
-      }
+      onClick={onStarClick}
     >
       <StarIcon />
     </IconButton>
