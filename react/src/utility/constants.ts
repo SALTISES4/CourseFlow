62432d77b3698d03a45391332cfecd24e85f07ba
Alexis Dray <alexis@littlebox.io>
2026-04-02 14:29:28 -0400

    continue cleanup

--- react/src/utility/constants.ts
+++ react/src/utility/constants.ts
@@ -1,46 +1,4 @@
-import { NumTuple } from '@cf/types/common'
-import { CfObjectType } from '@cf/types/enum'
-import ThemeHelper from '@cf/utility/ThemeHelper.class'
 import { _t } from '@cf/utility/Utility.class'
-
-/*
-Determines how long an action locks an object
-by default, in ms. Once the action ends, the lock
-is cleared (so this is a maximum time).
-*/
-export const lockTimes = {
-  move: 5000,
-  update: 5000,
-  select: 60000
-}
-
-export const nodeKeys = ['activity', 'course', 'program']
-
-export const columnwidth = 160
-
-export const nodePorts = {
-  source: {
-    e: [1, 0.6],
-    w: [0, 0.6],
-    s: [0.5, 1]
-  },
-  target: {
-    n: [0.5, 0],
-    e: [1, 0.4],
-    w: [0, 0.4]
-  }
-}
-
-export const portKeys = ['n', 'e', 's', 'w']
-
-export const portDirection: NumTuple[] = [
-  [0, -1],
-  [1, 0],
-  [0, 1],
-  [-1, 0]
-]
-
-export const portPadding = 10
 
 export const taskKeys = {
   0: '',
@@ -99,7 +57,6 @@ export const strategyKeys = {
   11: 'other'
 }
 
-// TODO: review all of this with MC
 export const defaultColumnSettings = {
   // activities
   0: { colour: '#6738ff', icon: 'other' },
@@ -122,41 +79,6 @@ export const defaultColumnSettings = {
   'new-column': { colour: '#bbb', icon: 'other' }
 }
 
-export const nodeTypeKeys = {
-  0: 'activity node',
-  1: 'course node',
-  2: 'program node'
-}
-
-// @todo this is redundant now
-export const objectDictionary = {
-  nodelink: CfObjectType.NODELINK,
-  node: CfObjectType.NODE,
-  week: CfObjectType.WEEK,
-  column: CfObjectType.COLUMN,
-  outcome: CfObjectType.OUTCOME,
-  outcomeBase: CfObjectType.OUTCOME,
-  workflow: CfObjectType.WORKFLOW,
-  outcomenode: CfObjectType.OUTCOMENODE
-}
-
-export const parentDictionary = {
-  nodelink: 'node',
-  node: 'week',
-  week: 'workflow',
-  column: 'workflow',
-  outcome: 'outcome',
-  outcomeBase: 'workflow'
-}
-
-export const throughParentDictionary = {
-  node: 'nodeweek',
-  week: 'weekworkflow',
-  column: 'columnworkflow',
-  outcome: 'outcomeoutcome',
-  outcomeBase: 'outcomeworkflow'
-}
-
 export const permissionKeys = {
   none: 0,
   view: 1,
@@ -165,44 +87,9 @@ export const permissionKeys = {
   student: 4
 }
 
-export const roleKeys = {
-  none: 0,
-  student: 1,
-  teacher: 2
-}
-
-export const defaultDropState = {
-  node: false,
-  week: true,
-  outcome: [true, false, false, false, false]
-}
-
 /*******************************************************
  * FUNCTIONS
  *******************************************************/
-
-
-/**
- * get all possible object sets
- **/
-export const objectSetsTypes = () => ({
-  'program outcome': ThemeHelper.capFirst(_t('program outcome')),
-  'course outcome': ThemeHelper.capFirst(_t('course outcome')),
-  'activity outcome': ThemeHelper.capFirst(_t('activity outcome')),
-  'program node': ThemeHelper.capFirst(_t('program node')),
-  'course node': ThemeHelper.capFirst(_t('course node')),
-  'activity node': ThemeHelper.capFirst(_t('activity node'))
-})
-
-// @todo no
-//missingTranslations, DO NOT DELETE. This will ensure that a few "utility" translations that don't otherwise show up get translated
-// function missingTranslations() {
-//   Utility.logger('missingTranslations called')
-//   _t('activity')
-//   _t('course')
-//   _t('program')
-//   _t('project')
-// }
 
 export const languageOptions = [
   {
