131bba5df0e9cd9c61e719f6c1c714202240b5ae
Aleksandar Nikolic <digrejzo@gmail.com>
2026-04-07 10:33:43 +0200

    Extract EmptyCell DND logic into a separate hook

--- react/src/components/views/WorkflowView/WorkflowEditView/components/Week/Cell/CellPhantom/index.tsx
+++ react/src/components/views/WorkflowView/WorkflowEditView/components/Week/Cell/CellPhantom/index.tsx
@@ -1,23 +1,9 @@
-import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
-import {
-  Edge,
-  attachClosestEdge,
-  extractClosestEdge
-} from '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge'
-import store from '@cfRedux/store'
-import { isGridCell } from '@cfViews/WorkflowView/WorkflowEditView/types'
 import { alpha } from '@mui/material'
 import Box from '@mui/material/Box'
-import { produce } from 'immer'
-import { useEffect, useState } from 'react'
 
 import DropIndicator from '../DropIndicator'
-import { WeekCellPhantomTypeInternal, WeekCellType } from '../types'
-
-// read from the store API to avoid expensive useSelector subscription
-function getIsRowInsert() {
-  return store.getState().workspace.node.insertMode === 'row'
-}
+import useEmptyCellDnd from './useEmptyCellDnd'
+import { WeekCellPhantomTypeInternal } from '../types'
 
 const WeekCellPhantom = ({
   columnId,
@@ -29,12 +15,13 @@ const WeekCellPhantom = ({
   emptyRow,
   onDrop
 }: WeekCellPhantomTypeInternal) => {
-  const [state, setState] = useState<{
-    draggedOver: boolean
-    closestEdge: Edge | null
-  }>({
-    draggedOver: false,
-    closestEdge: null
+  const dnd = useEmptyCellDnd({
+    wrapRef,
+    columnId,
+    coordsWeek,
+    coordsY,
+    emptyRow,
+    onDrop
   })
 
   // only highlighting the border when we're not highligting the full cell
@@ -43,100 +30,16 @@ const WeekCellPhantom = ({
   // only showing background if there are no active edges (border)
   // or we're supposed to do a cell highlight (from the row)
   const backgroundIndicator =
-    !state.closestEdge && (state.draggedOver || highlight === 'cell')
+    !dnd.closestEdge && (dnd.draggedOver || highlight === 'cell')
 
   // only showing border when there's the edge or we highlight it (from the row)
-  const lineIndicator = edgeIndicator || (state.closestEdge && !emptyRow)
-
-  useEffect(() => {
-    const el = wrapRef.current
-    if (!el) {
-      return null
-    }
-
-    return dropTargetForElements({
-      element: el,
-      getData: ({ input, element }) => {
-        return attachClosestEdge(
-          {},
-          {
-            input,
-            element,
-            allowedEdges: ['top', 'bottom']
-          }
-        )
-      },
-      onDrag: ({ source, self }) => {
-        if (!isGridCell(source.data)) {
-          return
-        }
-
-        if (
-          getIsRowInsert() &&
-          isGridCell(source.data) &&
-          source.data.coords.y !== coordsY
-        ) {
-          setState(
-            produce((draft) => {
-              draft.closestEdge = extractClosestEdge(self.data)
-            })
-          )
-        }
-      },
-      onDragEnter: ({ source, self }) => {
-        setState(
-          produce((draft) => {
-            draft.draggedOver = true
-            draft.closestEdge = null
-
-            if (
-              getIsRowInsert() &&
-              isGridCell(source.data) &&
-              source.data.coords.y !== coordsY
-            ) {
-              draft.closestEdge = extractClosestEdge(self.data)
-            }
-          })
-        )
-      },
-      onDragLeave: () => {
-        setState({
-          draggedOver: false,
-          closestEdge: null
-        })
-      },
-      canDrop: ({ source }) => isGridCell(source.data),
-      onDrop: ({ source }) => {
-        const dropped = source.data
-        if (!isGridCell(dropped)) {
-          return null
-        }
-
-        if (isGridCell(dropped)) {
-          onDrop({
-            type: WeekCellType.PHANTOM,
-            edge: undefined,
-            id: dropped.id,
-            fromWeek: dropped.coords.week,
-            toWeek: coordsWeek,
-            toColumn: columnId,
-            toRow: coordsY
-          })
-        }
-
-        setState({
-          draggedOver: false,
-          closestEdge: null
-        })
-      }
-    })
-  }, [wrapRef, columnId, coordsWeek, coordsY, emptyRow, onDrop])
+  const lineIndicator = edgeIndicator || (dnd.closestEdge && !emptyRow)
 
   return (
-    <Box sx={{ height: '100%' }}>
+    <Box style={{ height: '100%' }}>
       {backgroundIndicator && <DropIndicator color={alpha(borderColor, 0.2)} />}
       {lineIndicator && (
-        <DropIndicator edge={edgeIndicator || state.closestEdge} />
+        <DropIndicator edge={edgeIndicator || dnd.closestEdge} />
       )}
     </Box>
   )
